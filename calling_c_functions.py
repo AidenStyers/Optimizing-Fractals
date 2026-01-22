847544b4eb0e777bab145edd751023118b6cefc1
Aiden Styers <aiden.styers@gmail.com>
2026-01-22 17:55:17 -0600

    Finally updated the project sturcutre. Deleted all  old files used for fractal generation and analysis now that the GPU is being utilized. Updated .gitignore accordingly

--- calling_c_functions.py
+++
@@ -1,9 +0,0 @@
-from ctypes import *
-
-so_file = r"C:/Users/aiden/Fractals/fractal_generation.dll"
-
-my_functions = CDLL(so_file)
-
-fileName = "test3.png"
-
-my_functions.greyscaleFractal(fileName.encode('utf-8'), c_float(-2), c_float(2), c_float(-2), c_float(2), 1000, 1000, 50)
